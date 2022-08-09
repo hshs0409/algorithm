51aa59dbcca044a00cd36a5f1212b255c4aef7d2
hshs0409 <0409hshs@gmail.com>
2022-08-09 18:50:15 +0900

    docs: test1
    - test1 - test 2 \n - test3

---
+++ 22-07-03/16438.py
@@ -0,0 +1,15 @@
+N = int(input())
+result_list = [[] for _ in range(7)]
+num_list = [N]
+
+prev_monkey = 'A' * N
+# 무조건 앞을 크게
+print('A' * 25)
+
+for i in range(7):
+    new_num_list = num_list
+    for num in new_num_list:
+        num
+
+for result in result_list:
+    print(result)
